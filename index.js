7e2e4cb9964a08458aaf2c4d7ed377667fe1727a
Erd <edbz2k2@gmail.com>
2018-06-20 08:35:28 -0500

    separate demo and module

--- index.js
+++ index.js
@@ -1,65 +1,14 @@
 const createGrid = require('./utils/createGrid');
-const inquirer = require('inquirer');
 
-var questions = [
-    {
-      type: 'input',
-      name: 'inputFolder',
-      message: "Input folder",
-      default: 'input'
-    },
-    {
-      type: 'input',
-      name: 'outputFolder',
-      message: "output folder",
-      default: 'output'
-    },
-    {
-      type: 'input',
-      name: 'columns',
-      message: 'Columns (width)',
-      validate: function(value) {
-        var valid = !isNaN(parseFloat(value));
-        return valid || 'Please enter a number';
-      },
-      filter: Number,
-      default: 7
-    },
-    {
-      type: 'input',
-      name: 'rows',
-      message: 'Rows (height)',
-      validate: function(value) {
-        var valid = !isNaN(parseFloat(value));
-        return valid || 'Please enter a number';
-      },
-      filter: Number,
-      default: 5
-    },
-    {
-      type: 'input',
-      name: 'ppi',
-      message: 'PPI',
-      validate: function(value) {
-        var valid = !isNaN(parseFloat(value));
-        return valid || 'Please enter a number';
-      },
-      filter: Number,
-      default: 300
-    },
-    {
-      type: 'input',
-      name: 'diameterMM',
-      message: 'Jewel Diameter (mm)',
-      validate: function(value) {
-        var valid = !isNaN(parseFloat(value));
-        return valid || 'Please enter a number';
-      },
-      filter: Number,
-      default: 80
-    }
-];
-  
-inquirer.prompt(questions).then(answers => {
-    createGrid(answers)
-});
+/**
+ * @param {Object} params
+ * @param {Number} params.columns
+ * @param {Number} params.rows
+ * @param {Number} params.ppi
+ * @param {String} params.inputFolder
+ * @param {Number} params.diameterMM
+ * @param {String} params.outputFolder
+ */
+module.exports = (params) => {
+    return createGrid(params)
+}
