9e67179c15f1e6521829decf6b60207ed61807c9
Eric Dobroveanu <eric.dobroveanu@uptake.com>
2018-08-19 12:12:52 -0500

    get ready for publish

--- config/env-sample.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    accessKeyId: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 
-    secretAccessKey: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    bucket: '123bestbuck',
-    server: 'https://s3.us-east-2.amazonaws.com'
-}
